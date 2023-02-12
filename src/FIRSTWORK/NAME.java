1d2e274269d8d1f5fd4137099637d5aac542924c
Katerina Ryzhkova <Katerina_ryzhkova@bk.ru>
2023-02-13 02:41:46 +0300

    Initial commit

---
+++ src/FIRSTWORK/NAME.java
@@ -0,0 +1,14 @@
+package FIRSTWORK;
+
+public class NAME {
+        public static void main(String[] args) {
+            System.out.println("*     *       *      ***************   *      *         * ");
+            System.out.println("*   *       *   *           *            *  *         *   *");
+            System.out.println("* *        * * * *          *              *         * * * *");
+            System.out.println("*   *     *       *         *              *        *       *");
+            System.out.println("*     *  *         *        *              *       *         *");
+            System.out.println();
+            System.out.printf("847-92=");
+            System.out.println(847 - 92);
+        }
+}
